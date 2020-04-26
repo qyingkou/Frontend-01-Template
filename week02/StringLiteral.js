49bb345160694ca4667aa97ba2db70f53096b2f3
qyingkou <qyingkou@163.com>
2020-04-26 21:48:48 +0800

    modify about "lookahead" regexp

--- week02/StringLiteral.js
+++ week02/StringLiteral.js
@@ -13,25 +13,25 @@ function stringLiteral(literal) {
   const reg_DoubleStringCharacter = /^(")[^"\\\u000A\u000D\u2028\u2029]*\1$/u;
   const reg_StringCharacter = [
     /^[\u2028\u2029]$/, // <LS> or <PS>
-    /^\\([\u000A]|[\u2028\]|[\u2029]|[\u000D][\u000A]|(?<![\u000A])[\u000D])$/, // LineContinuation
+    /^\\([\u000A]|[\u2028\]|[\u2029]|[\u000D][\u000A]|[\u000D](?![\u000A]))$/, // LineContinuation
     /* \ EscapeSequence */
     [
       /^\\['"\\\b\f\n\r\t\v]|[1]$/u, // CharacterEscapeSequence
-      /^\\(?<![\d])0$/, // 0 [lookahead ∉ DecimalDigit]
+      /^\\0(?![\d])$/, // 0 [lookahead ∉ DecimalDigit]
       /^\\x[0-9a-fA-F]{2}$/, // HexEscapeSequence
       /^\\u(?:[0-9a-fA-F]{4}|u\{[\u0000-{10FFFF}]\})$/u, // UnicodeEscapeSequence
     ],
   ];
 
   return (
-    regTest(reg_SingleStringCharacter, value) ||
-    regTest(reg_DoubleStringCharacter, value) ||
-    regTest(reg_StringCharacter[0]) ||
-    regTest(reg_StringCharacter[1]) ||
-    regTest(reg_StringCharacter[2][0]) ||
-    regTest(reg_StringCharacter[2][1]) ||
-    regTest(reg_StringCharacter[2][2]) ||
-    regTest(reg_StringCharacter[2][3])
+    (regTest(reg_SingleStringCharacter, value) ||
+      regTest(reg_DoubleStringCharacter, value)) &&
+    (regTest(reg_StringCharacter[0]) ||
+      regTest(reg_StringCharacter[1]) ||
+      regTest(reg_StringCharacter[2][0]) ||
+      regTest(reg_StringCharacter[2][1]) ||
+      regTest(reg_StringCharacter[2][2]) ||
+      regTest(reg_StringCharacter[2][3]))
   );
 }
 function regTest(reg, value) {
