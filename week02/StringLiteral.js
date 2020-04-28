68870aa5416a2d8219b983324279a7b37584aecd
qyingkou <qyingkou@163.com>
2020-04-28 16:41:22 +0800

    mod Regexp logic

--- week02/StringLiteral.js
+++ week02/StringLiteral.js
@@ -1,37 +1,41 @@
 /* 
-Unicode CodePoint-Hex:
-
-LINE FEED <LF> U+000A 
-CARRIAGE RETURN <CR> U+000D
-LINE SEPARATOR <LS> U+2028
-PARAGRAPH SEPARATOR <PS> U+2029
+  ==== Unicode CodePoint-Hex: ====
+  LINE FEED <LF>:           U+000A 
+  CARRIAGE RETURN <CR>:     U+000D
+  LINE SEPARATOR <LS>:      U+2028
+  PARAGRAPH SEPARATOR <PS>: U+2029
 */
 function stringLiteral(literal) {
-  let flag = [];
   const value = literal.trim();
-  const reg_SingleStringCharacter = /^(')[^'\\\u000A\u000D\u2028\u2029]*\1$/u;
-  const reg_DoubleStringCharacter = /^(")[^"\\\u000A\u000D\u2028\u2029]*\1$/u;
+  const reg_SingleStringCharacter_sourcecode = /^(')[^'\\\u000A\u000D\u2028\u2029]*\1$/u;
+  const reg_DoubleStringCharacter_sourcecode = /^(")[^"\\\u000A\u000D\u2028\u2029]*\1$/u;
   const reg_StringCharacter = [
-    /^[\u2028\u2029]$/, // <LS> or <PS>
-    /^\\([\u000A]|[\u2028\]|[\u2029]|[\u000D][\u000A]|[\u000D](?![\u000A]))$/, // LineContinuation
+    /* <LS> or <PS> */
+    /^[\u2028\u2029]$/u,
+    /* LineContinuation */
+    /^\\([\u000A]|[\u2028\]|[\u2029]|[\u000D][\u000A]|[\u000D](?![\u000A]))$/u,
     /* \ EscapeSequence */
     [
-      /^\\['"\\\b\f\n\r\t\v]|[1]$/u, // CharacterEscapeSequence
-      /^\\0(?![\d])$/, // 0 [lookahead ∉ DecimalDigit]
-      /^\\x[0-9a-fA-F]{2}$/, // HexEscapeSequence
-      /^\\u(?:[0-9a-fA-F]{4}|u\{[\u0000-{10FFFF}]\})$/u, // UnicodeEscapeSequence
+      /* CharacterEscapeSequence */
+      /^\\['"\\\b\f\n\r\t\v]|[1]$/u,
+      /* 0 [lookahead ∉ DecimalDigit] */
+      /^\\0(?![\d])$/,
+      /* HexEscapeSequence */
+      /^\\x[0-9a-fA-F]{2}$/,
+      /* UnicodeEscapeSequence */
+      /^\\u(?:[0-9a-fA-F]{4}|u\{[\u0000-{10FFFF}]\})$/u,
     ],
   ];
 
   return (
-    (regTest(reg_SingleStringCharacter, value) ||
-      regTest(reg_DoubleStringCharacter, value)) &&
-    (regTest(reg_StringCharacter[0]) ||
-      regTest(reg_StringCharacter[1]) ||
-      regTest(reg_StringCharacter[2][0]) ||
-      regTest(reg_StringCharacter[2][1]) ||
-      regTest(reg_StringCharacter[2][2]) ||
-      regTest(reg_StringCharacter[2][3]))
+    regTest(reg_SingleStringCharacter_sourcecode, value) ||
+    regTest(reg_DoubleStringCharacter_sourcecode, value) ||
+    regTest(reg_StringCharacter[0]) ||
+    regTest(reg_StringCharacter[1]) ||
+    regTest(reg_StringCharacter[2][0]) ||
+    regTest(reg_StringCharacter[2][1]) ||
+    regTest(reg_StringCharacter[2][2]) ||
+    regTest(reg_StringCharacter[2][3])
   );
 }
 function regTest(reg, value) {
